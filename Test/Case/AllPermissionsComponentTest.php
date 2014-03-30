48fb775260b4042958bcff5492fffc91d3ea3dc5
Joris Blaak <joris@label305.com>
2014-03-30 16:12:23 +0200

    Added a .travis.yml for plugin testing

---
+++ Test/Case/AllPermissionsComponentTest.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * All PermissionsComponent Test
+ */
+class AllPermissionsComponentTest extends PHPUnit_Framework_TestSuite {
+
+	public static function suite() {
+		$suite = new CakeTestSuite('All PermissionsComponent plugin tests');
+		$path = App::pluginPath('PermissionsComponent');
+		$testPath = $path . DS . 'Test' . DS . 'Case';
+		if (!is_dir($testPath)) {
+			continue;
+		}
+
+		$iterator = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($testPath), RecursiveIteratorIterator::CHILD_FIRST);
+		foreach ($iterator as $folder) {
+			$folder = (string)$folder;
+			$folderName = basename($folder);
+
+			if ($folderName === '.' || $folderName === '..') {
+				continue;
+			}
+			$suite->addTestDirectory($folder);
+		}
+
+		$suite->addTestDirectory($testPath);
+		return $suite;
+	}
+
+}
